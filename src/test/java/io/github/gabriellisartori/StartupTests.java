6d93be53de0b84933100b5680f253dc703c216c1
Gabrielli Sartori <gabriellisartori@gmail.com>
2026-08-12 16:34:22 -0300

    first steps in Java with Spring Boot 3.4.0

---
+++ src/test/java/io/github/gabriellisartori/StartupTests.java
@@ -0,0 +1,13 @@
+package io.github.gabriellisartori;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
