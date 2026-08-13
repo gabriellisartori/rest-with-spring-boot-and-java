ceeeac9f2d234c174d4ee175956a4b0360145e5e
Gabrielli Sartori <gabriellisartori@gmail.com>
2026-08-13 15:02:12 -0300

    feature: learn about logs in spring boot

---
+++ src/main/java/io/github/gabriellisartori/class6_7/controllers/TestLogController.java
@@ -0,0 +1,22 @@
+package io.github.gabriellisartori.class6_7.controllers;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class TestLogController {
+
+    private Logger logger = LoggerFactory.getLogger(TestLogController.class);
+
+    @GetMapping("/test")
+    public String testLog() {
+        logger.debug("This is a debug log message");
+        logger.info("This is an info log message");
+        logger.warn("This is a warn log message");
+        logger.error("This is an error log message");
+        return "Logs generated successfully!";
+    }
+}
