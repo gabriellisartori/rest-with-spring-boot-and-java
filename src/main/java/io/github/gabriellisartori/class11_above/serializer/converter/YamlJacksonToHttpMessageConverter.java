23897a20d84314a67de4048699d8ba82fe61e729
Gabrielli Sartori <gabriellisartori@gmail.com>
2026-08-14 11:45:43 -0300

    learn about content negotiation

---
+++ src/main/java/io/github/gabriellisartori/class11_above/serializer/converter/YamlJacksonToHttpMessageConverter.java
@@ -0,0 +1,14 @@
+package io.github.gabriellisartori.class11_above.serializer.converter;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.AbstractJackson2HttpMessageConverter;
+
+public final class YamlJacksonToHttpMessageConverter extends AbstractJackson2HttpMessageConverter {
+    protected YamlJacksonToHttpMessageConverter() {
+        super(new YAMLMapper()
+                .setSerializationInclusion(JsonInclude.Include.NON_NULL),
+                MediaType.parseMediaType("application/yaml"));
+    }
+}
