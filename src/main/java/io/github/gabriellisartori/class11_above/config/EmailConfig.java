85fad1a5bf8c6c25624b3add24be8fda4e7578e4
Gabrielli Sartori <gabriellisartori@gmail.com>
2026-08-31 18:35:41 -0300

    feat: send email using gmail account

---
+++ src/main/java/io/github/gabriellisartori/class11_above/config/EmailConfig.java
@@ -0,0 +1,81 @@
+package io.github.gabriellisartori.class11_above.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Objects;
+
+@Configuration
+@ConfigurationProperties(prefix = "spring.mail")
+public class EmailConfig {
+
+    private String host;
+    private int port;
+    private String username;
+    private String password;
+    private String from;
+    private Boolean ssl;
+
+    public EmailConfig() {
+    }
+
+    public String getHost() {
+        return host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    public Boolean getSsl() {
+        return ssl;
+    }
+
+    public void setSsl(Boolean ssl) {
+        this.ssl = ssl;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        EmailConfig that = (EmailConfig) o;
+        return getPort() == that.getPort() && Objects.equals(getHost(), that.getHost()) && Objects.equals(getUsername(), that.getUsername()) && Objects.equals(getPassword(), that.getPassword()) && Objects.equals(getFrom(), that.getFrom()) && Objects.equals(getSsl(), that.getSsl());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getHost(), getPort(), getUsername(), getPassword(), getFrom(), getSsl());
+    }
+}
---
+++ src/main/java/io/github/gabriellisartori/class11_above/controllers/EmailController.java
@@ -0,0 +1,36 @@
+package io.github.gabriellisartori.class11_above.controllers;
+
+
+import io.github.gabriellisartori.class11_above.controllers.docs.EmailControllerDocs;
+import io.github.gabriellisartori.class11_above.data.dto.request.EmailRequestDTO;
+import io.github.gabriellisartori.class11_above.services.EmailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("api/email/v1")
+public class EmailController implements EmailControllerDocs {
+
+    @Autowired
+    private EmailService service;
+
+    @PostMapping
+    @Override
+    public ResponseEntity<String> sendEmail(@RequestBody EmailRequestDTO emailRequest) {
+        service.sendSimpleEmail(emailRequest);
+        return new ResponseEntity<>("E-mail sent with success", HttpStatus.OK);
+    }
+
+    @PostMapping(value = "/withAttachment", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @Override
+    public ResponseEntity<String> sendEmailWithAttachment(
+            @RequestParam("emailRequest") String emailRequest,
+            @RequestParam("attachment") MultipartFile attachment) {
+        service.sendEmailWithAttachment(emailRequest, attachment);
+        return new ResponseEntity<>("E-mail sent with success", HttpStatus.OK);
+    }
+}
