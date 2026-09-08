2013ba7dc443a679e8246eab862da1b8d26e57f4
Gabrielli Sartori <gabriellisartori@gmail.com>
2026-09-08 10:33:49 -0300

    feat: continuous integration with github actions and Amazon ECR and ECS

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# rest-with-spring-boot-and-java
+# Rest with Spring Boot and Java 21
+
+[![Continuous Integration and Delivery with GitHub Actions](https://github.com/gabriellisartori/rest-with-spring-boot-and-java/actions/workflows/continuous-deployment.yml/badge.svg)](https://github.com/gabriellisartori/rest-with-spring-boot-and-java/actions/workflows/continuous-deployment.yml)
--- src/main/java/io/github/gabriellisartori/class11_above/config/OpenApiConfig.java
+++ src/main/java/io/github/gabriellisartori/class11_above/config/OpenApiConfig.java
@@ -12,7 +12,7 @@ public class OpenApiConfig {
     OpenAPI customOpenAPI() {
         return new OpenAPI()
                 .info(new Info()
-                        .title("REST API Documentation: Spring Boot 2026 REST API's do 0 à AWS e GCP c Java e Docker")
+                        .title("REST API Documentation: Spring Boot 2026 REST API's do 0 à AWS e GCP com Java e Docker")
                         .version("v1")
                         .description("This is a sample Spring Boot RESTful service using OpenAPI 3.")
                 );
